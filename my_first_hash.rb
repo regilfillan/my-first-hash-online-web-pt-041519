76f97f82fbd1557a0f75e4fa86aebf3ac0f12d67
Ryan Gilfillan <regilfillan@gmail.com>
2019-04-27 22:19:00 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,5 @@
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
-  
+  we_are_family= {"partner" => "Nick", "youngest sister" => "Cameron", "middle sister" => "Kendall", "brother"=> "Chase"}
 end
 
 
